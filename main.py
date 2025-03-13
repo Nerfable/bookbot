063c54898061a3e080d8d2aef12471a8750883f9
Nerfable <mario.veit@hotmail.de>
2025-03-13 15:18:54 +0100

    Bookbook done

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import sys
+if  len(sys.argv)!=  2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 from stats import get_num_words, get_dict_num_characters, chars_dict_to_sorted_list
 
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
     num_words = get_num_words(book_text)
     dict_num_characters = get_dict_num_characters(book_text)
